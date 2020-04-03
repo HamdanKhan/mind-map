8b4dfdc814d51fe47343b6cf4e1a3298d01b1a4f
Hamdan <talktohamdan@gmail.com>
2020-04-03 14:34:37 +0500

    more words

---
+++ words/adjective.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1584370160062" ID="ID_160184349" MODIFIED="1584514673211" TEXT="adjective"/>
+</map>
